32e13e5c36cfb85cc1d69295a5282276a08a5b24
dreammo <dreammovip@gmail.com>
2022-02-12 13:35:58 +0800

    Update schema.lua

--- ip-access-forbidden/schema.lua
+++ ip-access-forbidden/schema.lua
@@ -7,7 +7,7 @@ return {
             config = {
                 type = "record",
                 fields = {
-                    { hosts = { type = "array", required = true, elements = typedefs.host }, },
+                    { hosts = { type = "array", required = true, elements = typedefs.host_with_optional_port }, },
                     { status_code = { type = "integer", required = false, default = 403 } },
                     { tip_message = { type = "string", required = false, default = "Access Forbidden" } }
                 },
