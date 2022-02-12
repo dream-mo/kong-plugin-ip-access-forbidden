47e47c539cea9a24dc5e023f036e380d76e8b43f
dreammo <dreammovip@gmail.com>
2022-02-12 13:20:49 +0800

    Update handler.lua

--- ip-access-forbidden/handler.lua
+++ ip-access-forbidden/handler.lua
@@ -4,10 +4,11 @@ IpAccessForbiddenHandler.PRIORITY = 1
 IpAccessForbiddenHandler.VERSION = "0.1.0"
 
 function IpAccessForbiddenHandler:access(conf)
-    local ip_addresses = conf.ip_addresses
+    local hosts = conf.hosts
     local host = kong.request.get_header('Host')
+    kong.log("Host: " .. host)
     kong.ctx.plugin.is_ip_access = false
-    for index, ip in pairs(ip_addresses) do
+    for index, ip in pairs(hosts) do
         if ip == host then
             kong.ctx.plugin.is_ip_access = true
             break
@@ -17,6 +18,7 @@ end
 
 function IpAccessForbiddenHandler:response(conf)
     local is_ip_access = kong.ctx.plugin.is_ip_access
+    kong.log("Intercept result: ", is_ip_access)
     if (is_ip_access) then
         kong.response.exit(conf.status_code, conf.tip_message)
     end
